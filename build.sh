57c7c298e683c762f6f7efb8a0752b7d35f7188f
BuffOvrFlw <ffrick0001980@proton.me>
2026-06-28 18:13:09 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -4,15 +4,7 @@ set -eu
 
 CWD=$(pwd)
 
-# if [ -n "${1:+x}" ]; then
-# 	if [ "$1" = "clean" ]; then
-# 		echo "Cleaning mbedtls"
-# 		(cd mbedtls && git reset --hard && git clean -fdx && cd framework && git reset --hard && git clean -fdx)
-# 		echo "Cleaning zlib"
-# 		(cd zlib && git reset --hard && git clean -fdx)
-# 	fi
-# fi
-
+(cd native/ && git submodule update --init --recursive)
 (cd $CWD/native/ && ./compile-native.sh)
 (cd $CWD && mvn clean install)
-(cd $CWD/test_run/ && cp ../XenonCord.jar ./)
+(mkdir $CWD/test_run/ && cd $CWD/test_run/ && cp ../XenonCord.jar ./)
